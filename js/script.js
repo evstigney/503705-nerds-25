8d6d266fd0083536770bc2bdff6a40015a4663f9
evs <evstigney2010@gmail.com>
2019-06-10 21:53:41 +0300

    слайдер на гл странице оживила

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/* ОЖИВЛЯЕМ СЛАЙДЕР */
+
+var showSlider = function () {
+    var target = event.target;
+    console.log(target);
+    if (target.classList.contains('slider-button-current')) {
+      return;
+    } else {
+      var sliderButtonsArr = document.querySelectorAll('.slider-button');
+      var slidersArr = document.querySelectorAll('.main-feature-item');
+      for (var i = 0; i < sliderButtonsArr.length; i++) {
+        var currentButton = sliderButtonsArr[i];
+        var currentSlider = slidersArr[i];
+        if (currentButton.classList.contains('slider-button-current')) {
+          currentButton.classList.remove('slider-button-current');
+          currentSlider.classList.remove('current-slider')
+          break;
+        }
+      }
+      for (var i = 0; i < sliderButtonsArr.length; i++) {
+        if (sliderButtonsArr[i] === target) {
+          var sliderIndex = i;
+        }
+      }
+      target.classList.add('slider-button-current');
+      slidersArr[sliderIndex].classList.add('current-slider');
+    }
+};
