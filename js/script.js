634c2eefc7240ff1af9dc9c65021854e09c8141c
Василиса <evstigney2010@gmail.com>
2019-06-11 11:47:18 +0300

    модальное окно появляется с анимацией

--- js/script.js
+++ js/script.js
@@ -1,27 +1,25 @@
-/* ОЖИВЛЯЕМ СЛАЙДЕР */
+/* --------- ПОКАЗЫВАЕМ ОКНО ОБРАТНОЙ СВЯЗИ -----------*/
 
-var showSlider = function () {
-    var target = event.target;
-    if (target.classList.contains('slider-button-current')) {
-      return;
-    } else {
-      var sliderButtonsArr = document.querySelectorAll('.slider-button');
-      var slidersArr = document.querySelectorAll('.main-feature-item');
-      for (var i = 0; i < sliderButtonsArr.length; i++) {
-        var currentButton = sliderButtonsArr[i];
-        var currentSlider = slidersArr[i];
-        if (currentButton.classList.contains('slider-button-current')) {
-          currentButton.classList.remove('slider-button-current');
-          currentSlider.classList.remove('current-slider')
-          break;
-        }
-      }
-      for (var i = 0; i < sliderButtonsArr.length; i++) {
-        if (sliderButtonsArr[i] === target) {
-          var sliderIndex = i;
-        }
-      }
-      target.classList.add('slider-button-current');
-      slidersArr[sliderIndex].classList.add('current-slider');
+var openModalWriteUs = function () {
+  var modalWindow = document.querySelector('.modal-write-us');
+  modalWindow.style.top = '-100%';
+  modalWindow.style.display= 'flex';
+  var windowPosition = parseInt(modalWindow.style.top);
+  setTimeout(function () {
+    while(windowPosition < 50) {
+      windowPosition += 1;
+      modalWindow.style.top = windowPosition + '%';
     }
+  }, 100);
+};
+
+var closeModalWriteUs = function () {
+  var modalWindow = document.querySelector('.modal-write-us');
+  var windowPosition = parseInt(modalWindow.style.top);
+  setTimeout(function () {
+    while (windowPosition > -100) {
+    windowPosition -= 1;
+    modalWindow.style.top = windowPosition + '%';
+    }
+  }, 1);
 };
